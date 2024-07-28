86ca2f4a28a354da417bc5b5e9b0c987a2dea23c
Hesham Sayed Abdul Rauof <153128708+Heshamsayed103@users.noreply.github.com>
2024-07-28 22:16:58 +0300

    Add files via upload

---
+++ Project 2.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int H = 72;
+	char b = 'G';
+
+	cout << "Please inter the number or char " << endl;
+	cin >> H;
+	cout << "The character  by ASCII is " << char(H);
+	cin >> b;
+	cout << "The number by ASCII = " << int(71);
+
+	return 0;
+
+
+}
