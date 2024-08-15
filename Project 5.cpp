12cf18a2f4e5158196491220ceb83cbf8765bdf8
Hesham Sayed Abdul Rauof <153128708+Heshamsayed103@users.noreply.github.com>
2024-08-15 15:02:11 +0300

    Add files via upload

---
+++ Project 5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+        cout << "LINE ONE" <<endl;
+        cout << "Line Two" <<endl;
+        cout << "Line Three" <<endl;
+         
+        cout << "I'm learning programming" << endl << "With" << endl << "Elzero" << endl << "Web" << endl << "School" << endl;
+        cout << "|=============================|" << endl << "|=== Elzero Web School ===|" << endl << "|=== C++ ===|" << endl << "|=====================================|";
+        return 0;
+
+}
