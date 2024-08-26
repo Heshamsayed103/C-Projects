642aea1a9a3ac467fdee5cbeda2defe8b5f32fd9
Hesham Sayed Abdul Rauof <153128708+Heshamsayed103@users.noreply.github.com>
2024-08-26 12:49:45 +0300

    Add files via upload

---
+++ Project 6.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double salary = 500.98;
+    cout << "bytes =" << sizeof(double) << endl;
+    cout << "bites =" << sizeof(double) * 8 << endl;
+
+    return 0;
+}
