d1174b30e8eeb06cb6cac7139741c9630354740c
Hesham Sayed Abdul Rauof <153128708+Heshamsayed103@users.noreply.github.com>
2024-08-28 21:10:27 +0300

    Add files via upload

---
+++ Project 7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+int main()
+{
+    cout<<"Maximum Integar number is ="<<INT_MAX<<endl;
+    cout<<"Manimum Integar number is ="<<INT_MIN<<endl;
+    cout<<"Maximum Short Integar number is ="<<SHRT_MAX<<endl;
+    cout <<"Manimum Short Integar number is ="<<SHRT_MAX<<endl;
+    
+    return 0;
+}
